189dee090805ef346aba398aaa6b1e301a33f902
Guillaume DG <Guillaume.dusseuxgicquel@gmail.com>
2019-02-19 15:23:27 +0100

    tp pagination à faire

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -14,7 +14,7 @@ export class AccueilComponent implements OnInit {
 
     this.collegue = [
       {pseudo:"Simone", score: 9, photoURL: "https://i.pinimg.com/originals/b4/1b/9a/b41b9a2a5eed598dbf36aaea9e1cdf98.jpg"},
-      {pseudo:"Gretta", score: 5, photoURL: "https://i.imgur.com/bnsF26a.gif"},
+      {pseudo:"Gretta", score: 5, photoURL: "https://i.pinimg.com/originals/06/83/f9/0683f931563b3d2e1ea8e0b389849c54.jpg"},
       {pseudo:"Olive", score: 4, photoURL: "https://thumbs.dreamstime.com/b/punk-rock-woman-2314557.jpg"},
       {pseudo:"Geraldine", score: -8, photoURL: "https://image1.masterfile.com/getImage/NjEwMi0wODgwMDE5OGVuLjAwMDAwMDAw=ALBU-5/6102-08800198en_Masterfile.jpg"},
       {pseudo:"Clitorine", score: 6, photoURL: "https://photos.travelblog.org/Photos/103181/523077/f/5360237-Pretty_punk_girl-0.jpg"},
