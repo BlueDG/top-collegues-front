2c55f781e97a245f9fc87d80ab7829cef5b3994d
Guillaume DG <Guillaume.dusseuxgicquel@gmail.com>
2019-02-20 12:08:33 +0100

    pause du tp

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -19,7 +19,7 @@ export class AccueilComponent implements OnInit {
       {pseudo:"Gretta", score: 5, photoURL: "https://i.pinimg.com/originals/06/83/f9/0683f931563b3d2e1ea8e0b389849c54.jpg"},
       {pseudo:"Olive", score: 4, photoURL: "https://thumbs.dreamstime.com/b/punk-rock-woman-2314557.jpg"},
       {pseudo:"Geraldine", score: -8, photoURL: "https://image1.masterfile.com/getImage/NjEwMi0wODgwMDE5OGVuLjAwMDAwMDAw=ALBU-5/6102-08800198en_Masterfile.jpg"},
-      {pseudo:"Clitorine", score: 6, photoURL: "https://photos.travelblog.org/Photos/103181/523077/f/5360237-Pretty_punk_girl-0.jpg"},
+      {pseudo:"Céline", score: 6, photoURL: "https://photos.travelblog.org/Photos/103181/523077/f/5360237-Pretty_punk_girl-0.jpg"},
       {pseudo:"Huguette", score: -4, photoURL: "https://media.npr.org/assets/img/2015/11/17/111315_annebernays_32_sq-1f03be97a66e01d88060887ebad37b4fe7502b99-s800-c85.jpg"}
     ];
 
